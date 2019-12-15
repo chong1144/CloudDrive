cc5b5535ddfde3ff14775631eac27300c50a8421
SenW <1144165853@qq.com>
2019-12-15 23:09:11 +0800

    update

--- project/Top.cpp
+++ project/Top.cpp
@@ -1,9 +1,12 @@
 #include "Package/Package.h"
 #include "Utility/Config.h"
+#include "Utility/Log.h"
 
 int main(int argc, char const *argv[])
 {
     Config c("../config.conf");
+
+    Log l("../Log/test.log");
 
     return 0;
 }
