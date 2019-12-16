769ae9588ab573490422e86f81dcaccc9d7b125c
huxuan666 <huxuan0307@tongji.edu.cn>
2019-12-16 21:13:10 +0800

    update uploader

--- Project/download/Uploader.h
+++ Project/download/Uploader.h
@@ -10,14 +10,16 @@
 #include <fstream>
 #include <fcntl.h>
 #include <sys/socket.h>
-#include <sys/epoll.h>
+
 #include <openssl/md5.h>
+
 #include <string.h>
 #include <ctime>
 #include <map>
-#include <mysql/mysql.h>
 #include <unistd.h>
-#include "../Utility/Log.h"
+#include <sys/epoll.h>
+#include <algorithm>
+
 using std::to_string;
 using size_t = uint64_t;
 using pType = PackageType;
@@ -25,7 +27,26 @@ const size_t headlen = sizeof (UniformHeader);
 using std::queue;
 const int MAXEVENTS = 20;
 
+int epoll_add (int epfd, int fd, uint32_t events)
+{
+	static epoll_event ep_ev;
+	ep_ev.data.fd = fd;
+	ep_ev.events = events;
+	if (-1 == epoll_ctl (epfd, EPOLL_CTL_ADD, ep_ev.data.fd, &ep_ev)) {
+		perror ("epoll_ctl()");
+		return -1;
+	}
+	return 0;
+}
 
+int epoll_del (int epfd, int fd)
+{
+	if (-1 == epoll_ctl (epfd, EPOLL_CTL_DEL, fd, nullptr)) {
+		perror ("epoll_ctl()");
+		return -1;
+	}
+	return 0;
+}
 
 class Uploader
 {
@@ -52,8 +73,8 @@ class Uploader
 	int fifo_ctrl_r;
 
 	//readfile model to/from upload model
-	int fifo_readfile_w;
-	int fifo_readfile_r;
+	//int fifo_readfile_w;
+	//int fifo_readfile_r;
 
 
 	UniformHeader head;
@@ -62,54 +83,115 @@ class Uploader
 	UploadFetchBody fetch;
 	UploadPushBody push;
 	Log fileLog;
+	//config
+	Config config;
+	// epollfd
+	int epfd;
+	epoll_event ep_events[MAXEVENTS];
+
+
 public:
+	Uploader (string config_file, string log_file) :config (config_file), fileLog (log_file)
+	{
+		epfd = epoll_create1 (0);
+		if (epfd == -1) {
+			fileLog.writeLog (Log::ERROR, string ("in run: epoll_create1 error") + strerror (errno));
+			exit (-1);
+		}
+		//open fifo
+		fifo_db_w = open (config.getValue ("Global", "path_fifo_rtod").c_str (), O_WRONLY);
+		fifo_db_r = open (config.getValue ("Global", "path_fifo_dtor").c_str (), O_RDONLY);
+		fifo_ctrl_w = open (config.getValue ("Global", "path_fifo_rtoc").c_str (), O_WRONLY);
+		fifo_ctrl_r = open (config.getValue ("Global", "path_fifo_ctor").c_str (), O_RDONLY);
+	}
+
 	// 向DB模块发送获取文件信息的请求
 	int sendReqFileInfotoDB (fileHash hashcode)
 	{
 
 	};
 
-	void addMap (fileHash hashcode, socket_t sockclnt)
+	void EpollAdd (int fd, uint32_t events)
 	{
-		for (auto sock : fs_map[hashcode]) {
-			// 已经在file-socket表中
-			if (sock.sock == sockclnt)
-				return;
+		fileLog.writeLog (Log::INFO, "EpollAdd");
+		static epoll_event ep_ev;
+		ep_ev.data.fd = fd;
+		ep_ev.events = events;
+		if (-1 == epoll_ctl (epfd, EPOLL_CTL_ADD, ep_ev.data.fd, &ep_ev)) {
+			perror ("epoll_ctl()");
+			fileLog.writeLog (Log::ERROR, string ("epoll_ctl()") + strerror (errno));
 		}
-		fs_map[hashcode].push_back ({ sockclnt,SOCKETSTATE::IDEL });
 	}
 
-	// 从控制模块获取命令
-	int getReqfromCtrl ()
+	int EpollWait ()
 	{
+		fileLog.writeLog (Log::INFO, "epollwait begin");
+		int cnt;
+		cnt = epoll_wait (epfd, this->ep_events, MAXEVENTS, -1);
+		if (-1 == cnt) {
+			perror ("epoll_wait()");
+			fileLog.writeLog (Log::ERROR, string ("epoll_wait()") + strerror (errno));
+		}
+		fileLog.writeLog (Log::INFO, string ("epollwait end, cnt: ") + to_string (cnt));
+		return cnt;
+	}
+
+	void Epolldel (int fd)
+	{
+		fileLog.writeLog (Log::INFO, "Epolldel");
+		if (-1 == epoll_ctl (epfd, EPOLL_CTL_DEL, fd, nullptr)) {
+			perror ("epoll_ctl()");
+			fileLog.writeLog (Log::ERROR, string ("epoll_ctl()") + strerror (errno));
+		}
+	}
+
+	void addfsTable (fileHash hashcode, socket_t sockclnt)
+	{
+		fileLog.writeLog (Log::INFO, string ("addfsTable begin") + to_string (sockclnt));
+		for (auto sock : fs_map[hashcode]) {
+			// 已经在file-socket表中
+			if (sock.sock == sockclnt) {
+				fileLog.writeLog (Log::WARNING, string ("socket already exists") + to_string (sockclnt));
+				return;
+			}
+		}
+		fileLog.writeLog (Log::INFO, string ("add a new file-socket to fsTable"));
+		fs_map[hashcode].push_back ({ sockclnt,SOCKETSTATE::IDEL });
+		fileLog.writeLog (Log::INFO, string ("addfsTable end") + to_string (sockclnt));
+	}
+
+	// 从控制模块获取上传请求
+	int handleReqfromCtrl ()
+	{
+		fileLog.writeLog (Log::INFO, string ("handleReqfromCtrl begin"));
 		int len;
-		int sckt;
+		int sockclnt;
 		if ((len = read (fifo_ctrl_r, &head, headlen)) != headlen) {
-			fileLog.writeLog (Log::ERROR, string ("uploader 读取header from Ctrl长度错误 len: ") + to_string (len));
+			fileLog.writeLog (Log::ERROR, string ("handleReqfromCtrl 读取header from Ctrl长度错误 len: ") + to_string (len));
 			return -1;
 		}
-
 
 		// 是上传文件请求
 		if (head.p == pType::UPLOAD_REQ) {
 			if ((len = read (fifo_ctrl_r, &req, head.len)) != head.len) {
-				fileLog.writeLog (Log::ERROR, string ("uploader 读取reqBody from Ctrl长度错误 len: ") + to_string (len));
+				fileLog.writeLog (Log::ERROR, string ("handleReqfromCtrl 读取reqBody from Ctrl长度错误 len: ") + to_string (len));
 				return -1;
 			}
 			// need a socket from ctrl!!!!!
-			if ((len = read (fifo_ctrl_r, &sckt, sizeof (int))) != 4) {
-				fileLog.writeLog (Log::ERROR, string ("uploader 读取socket from Ctrl错误 len: ") + to_string (len));
+			if ((len = read (fifo_ctrl_r, &sockclnt, sizeof (int))) != 4) {
+				fileLog.writeLog (Log::ERROR, string ("handleReqfromCtrl 读取socket from Ctrl错误 len: ") + to_string (len));
 				return -1;
 			}
-			addMap (req.MD5, sckt);
+			// 将文件和socket关联添加到fs表
+			addfsTable (req.MD5, sockclnt);
 			return 0;
+			fileLog.writeLog (Log::INFO, string ("handleReqfromCtrl done"));
+
 		}
 		else {
-			fileLog.writeLog (Log::ERROR, string ("uploader 接收到来自ctrl的错误请求"));
+			fileLog.writeLog (Log::ERROR, string ("handleReqfromCtrl 接收到来自ctrl的错误请求"));
 			return -1;
 		}
-
-
 	}
 
 	int getPackageFromClient (socket_t sockclnt)
@@ -137,6 +219,19 @@ public:
 			exit (-1);
 		}
 		epoll_event ep_events[MAXEVENTS];
+		// 初始监视控制模块的信号
+		EpollAdd (this->fifo_ctrl_r, EPOLLIN);
+		int cnt = EpollWait ();
+		fileLog.writeLog (Log::INFO, string ("epollwait end"));
+
+		for (int i = 0; i < cnt; ++i) {
+			auto& ep_ev = this->ep_events[i];
+			if (ep_ev.data.fd == fifo_ctrl_r) {
+				// 从控制端来的包
+				handleReqfromCtrl ();
+			}
+		}
+
 
 
 
